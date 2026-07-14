422c7272f44b97e89655612ec04a25228ebc1c4d
Nazish1712 <nazish1712@gmail.com>
2026-07-14 17:13:56 +0530

    made svg component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SvgComponent from "./components/SvgComponent"
+
+function App() {
+ 
+
+  return (
+    <main className="h-screen max-w-4xl mx-auto flex items-center flex-col my-20">
+      <SvgComponent/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/SvgComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SvgComponent = () => {
+  return (
+    <div className="h-20 w-20 bg-grey-50 rounded-md shadow-sm flex items-center justify-center">SvgComponent</div>
+  )
+}
+
+export default SvgComponent
